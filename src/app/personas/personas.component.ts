fd76ae03b94197932fa461b2c8fd92d388c6ae5d
cesaineam <cesaineam@unal.edu.co>
2023-07-14 16:53:36 -0500

    Binding en angular

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
 
 })
 export class PersonasComponent{
+    deshabilitar = false;
+    mensaje="No se ha agregado ninguna persona";
 
+    activarPersona(){
+        this.mensaje = "Se ha agregado persona"
+    }
 }
